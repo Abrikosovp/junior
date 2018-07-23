bc71e6fe1fd15483ed568f9339f49f3921bc22e2
Pavel Abrikosov <abrikosovp@mail.ru>
2018-07-23 19:18:46 +0300

    Типы данных. Calculate

--- section_001/src/main/java/ru/shifu/Calculate.java
+++ section_001/src/main/java/ru/shifu/Calculate.java
@@ -1,4 +1,26 @@
 package ru.shifu;
 
+/**
+ * Calculate.
+ *
+ * @author Pavel Abrikosov (abrikosovp@mail.ru).
+ * @version 1.
+ * @since 23.06.2018.
+ */
 public class Calculate {
+    /**
+     * Main.
+     * @param arg = arg.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    /**
+     * Метод echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
 }
--- section_001/src/test/java/ru/shifu/CalculateTest.java
+++ section_001/src/test/java/ru/shifu/CalculateTest.java
@@ -1,4 +1,23 @@
 package ru.shifu;
+import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+ * CalculateTest.
+ *
+ * @author Pavel Abrikosov (abrikosovp@mail.ru).
+ * @version 1.
+ * @since 23.06.2018.
+ */
 public class CalculateTest {
+    /**
+     * Метод echo.
+     */
+    @Test
+    public void thenEcho() {
+        Calculate calculate = new Calculate();
+        String result = calculate.echo("Павел");
+        assertThat(result, is("Echo, echo, echo: Павел"));
+    }
 }
