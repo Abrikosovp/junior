de6215b8840b83043e6e0ccbe75e2e89a10d6e76
Pavel Abrikosov <abrikosovp@mail.ru>
2018-07-24 12:29:46 +0300

    Типы данных - Расстояние между точками в системе координат

---
+++ section_001/src/main/java/ru/shifu/condition/Point.java
@@ -0,0 +1,25 @@
+package ru.shifu.condition;
+/**
+ * FitTest.
+ *
+ * @author Pavel Abrikosov (abrikosovp@mail.ru).
+ * @version 1.
+ * @since 24.06.2018.
+ */
+public class Point {
+    private int x;
+    private int y;
+
+    public  Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    /**
+     * Метод расчитывающий расстояние между точками в системе координат.
+     * @param that координаты.
+     * @return дистанцию между точками.
+     */
+    public double distanceTo(Point that) {
+        return Math.sqrt(Math.pow(that.x - this.x, 2) + Math.pow(that.y - this.y, 2));
+    }
+}
