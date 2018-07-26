5966b718f5f290c6bcf918c5a11fc4a07a863408
Pavel Abrikosov <abrikosovp@mail.ru>
2018-07-26 16:45:52 +0300

    Рефакторинг (Крестики-нолики на JavaFX)

--- section_001/src/main/java/ru/shifu/tictactoe/Logic3T.java
+++ section_001/src/main/java/ru/shifu/tictactoe/Logic3T.java
@@ -1,4 +1,7 @@
 package ru.shifu.tictactoe;
+
+import java.util.function.Predicate;
+
 /**
  *  Класс Logic3T отвечает за проверку логики.
  *
@@ -14,46 +17,56 @@ public class Logic3T {
     }
 
     /**
-     * Проверяет выигрышные комбинации для X.
-     * @return true если победил X и false если не победил
+     * Общий , универсальный метод, который вычисляет все последовательности.
+     * @param predicate  это параметризованное поведение. Мы его вводим, так как у нас два события: проверка на X и О.
+     * @param startX начальная точка.
+     * @param startY начальная точка
+     * @param deltaX движение.
+     * @param deltaY движение.
+     * @return true если есть победитель  или false если нет.
      */
-    public boolean isWinnerX() {
-        boolean result = false;
-        for (int i = 0; i < this.table.length - 1; i++) {
-            if (this.table[i][i].hasMarkX() == this.table[i + 1][i + 1].hasMarkX()
-                    && this.table[this.table.length - i - 1][i].hasMarkX() == this.table[this.table.length - i - 2][i + 1].hasMarkX()) {
-                result = true;
-            } else {
-                for (int j = 0; j < this.table.length - 1; j++) {
-                    if (this.table[j][i].hasMarkX() == this.table[j + 1][i].hasMarkX() &&
-                            this.table[i][j].hasMarkX() == this.table[i][j + 1].hasMarkX()) {
-                        result = true;
-                    }
-                }
+    public boolean fillBy(Predicate<Figure3T> predicate, int startX, int startY, int deltaX, int deltaY) {
+        boolean result = true;
+        for (int index = 0; index != this.table.length; index++) {
+            Figure3T cell = this.table[startX][startY];
+            startX += deltaX;
+            startY += deltaY;
+            if (!predicate.test(cell)) {
+                result = false;
+                break;
             }
         }
         return result;
     }
+
+    /**
+     * Проверяет выигрышные комбинации для X.
+     * @return true если победил X и false если не победил
+     */
+    public boolean isWinnerX() {
+                           return this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 0)
+                               || this.fillBy(Figure3T::hasMarkX, 0, 0, 0, 1)
+                               || this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 1)
+                               || this.fillBy(Figure3T::hasMarkX, this.table.length - 1, 0, -1, 1)
+                               || this.fillBy(Figure3T::hasMarkX, 1, 0, 0, 1)
+                               || this.fillBy(Figure3T::hasMarkX, 2, 0, 0, 1)
+                               || this.fillBy(Figure3T::hasMarkX, 0, 1, 1, 0)
+                               || this.fillBy(Figure3T::hasMarkX, 0, 2, 1, 0);
+
+                    }
     /**
      * Проверяет выигрышные комбинации для О.
      * @return true если победил О и false если не победил
      */
     public boolean isWinnerO() {
-        boolean result = false;
-        for (int i = 0; i < this.table.length -1; i++) {
-            if (this.table[i][i].hasMarkO() == this.table[i + 1][i + 1].hasMarkO()
-                    && this.table[this.table.length - i - 1][i].hasMarkO() == this.table[this.table.length - i - 2][i + 1].hasMarkO()) {
-                result = true;
-            } else {
-                for (int j = 0; j < this.table.length - 1; j++) {
-                    if (this.table[i][j].hasMarkO() == this.table[i][j + 1].hasMarkO()
-                            && this.table[j][i].hasMarkO() == this.table[j + 1][i].hasMarkO()) {
-                        result = true;
-                    }
-                }
-            }
-        }
-        return result;
+               return this.fillBy(Figure3T::hasMarkO, 0, 0, 0, 1)
+                   || this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 0)
+                   || this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 1)
+                   || this.fillBy(Figure3T::hasMarkO, 0, this.table.length - 1, 1, -1)
+                   || this.fillBy(Figure3T::hasMarkO, 1, 0, 0, 1)
+                   || this.fillBy(Figure3T::hasMarkO, 2, 0, 0, 1)
+                   || this.fillBy(Figure3T::hasMarkO, 0, 1, 1, 0)
+                   || this.fillBy(Figure3T::hasMarkO, 0, 2, 1, 0);
     }
 
     /**
@@ -64,7 +77,7 @@ public class Logic3T {
         boolean result = false;
         for (int i = 0; i < this.table.length; i++) {
             for (int j = 0; j < this.table.length; j++) {
-                if (!table[i][j].hasMarkO() && !table[i][j].hasMarkX()) {
+                if (table[i][j]!= null) {
                     result = true;
                     break;
                 }
