9922fc43174a6f58f9220210b2684b578dc2545a
booger2 <abrikosovp@mail.ru>
2019-02-08 02:24:24 +0300

    Заполнить динамически таблицу.

--- section_008/src/main/webapp/scripts/load.js
+++ section_008/src/main/webapp/scripts/load.js
@@ -1,4 +1,21 @@
-function validate() {
+function addRow() {
+    var fname = $('#fname');
+    var lname = $('#lname');
+    var desc = $('#desc');
+    var sex = $('input[name="sex"]:checked');
+
+    var arr = [fname, lname, desc];
+
+    if (validate(arr, sex)) {
+        $('#table tr:last').after(
+            '<tr><td>' + fname.val() + '</td>' +
+            '<td>' + lname.val() + '</td>'+
+            '<td>' + desc.val() + '</td>' +
+            '<td>' + sex.val() +'</td></tr>');
+    }
+}
+
+function validate(arr, sex) {
     var result = true;
     var fname = $('#fname');
     var lname = $('#lname');
